3663b6b93d4e40e77131f51ccaa1b11c61a8c42f
Paulo <paulo.1999sep@gmail.com>
2026-03-16 23:38:44 -0300

    exercicio 4

---
+++ 4/index.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <form action="ex4.php" method="post">
+
+
+<label for="">Cº</label>
+<input type="text" name="C">    
+<input type="submit" value="Enviar">
+<input type="reset" value="Limpar">
+    </form>
+
+    
+ </body>
+ </html>
