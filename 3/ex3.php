a84a7d94b3c5b13cb5bc644eb7698cad1d7fb9e1
Paulo <paulo.1999sep@gmail.com>
2026-03-17 20:36:46 -0300

    ex3

--- 3/ex3.php
+++ 3/ex3.php
@@ -1,8 +1,8 @@
 <?php
 
-$Nota1 = $_POST ["Nota1"];
-$Nota2 = $_POST ["Nota2"];
-$Nota3 = $_POST ["Nota3"];
+$Nota1 = $_POST ['N1'];
+$Nota2 = $_POST ['N2'];
+$Nota3 = $_POST ['N3'];
 $soma = $N1+$N2+$N3;
 $media =  $soma*3;
 $final = $media;
