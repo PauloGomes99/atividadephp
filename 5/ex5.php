4fc3c8ff448001f8ff2801095d6fb9c3e2a3e463
Paulo <paulo.1999sep@gmail.com>
2026-03-17 20:17:54 -0300

    ex5

---
+++ 5/ex5.php
@@ -0,0 +1,9 @@
+<?php
+
+$n = $_POST;
+$r = $_POST;
+$r = $n+2.2;
+
+echo "o valor é:".$r;
+
+?>
--- 5/index.php
+++ 5/index.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="ex5.php" method="post">
+
+
+<label for="">Insira um numero</label>
+<input type="text" name="n">    
+<input type="submit" value="Enviar">
+<input type="reset" value="Limpar">
+    </form>
 </body>
 </html>
