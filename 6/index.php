cd2a3b23c52647d00453a1ae2c4e10ffb7e0fc4b
Paulo <paulo.1999sep@gmail.com>
2026-03-17 20:38:05 -0300

    ex6

---
+++ 6/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ex6.php" method="post">
+    <label for="">Dolar US$</label>
+<input type="text" name="d">    
+<input type="submit" value="Enviar">
+<input type="reset" value="Limpar">
+    </form>
+    
+</body>
+</html>
