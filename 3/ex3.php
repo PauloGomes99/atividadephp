44af57c21a71d2765785b039742cfac873b10329
Paulo <paulo.1999sep@gmail.com>
2026-03-21 23:26:30 -0300

    ex3

--- 3/ex3.php
+++ 3/ex3.php
@@ -4,14 +4,15 @@ $Nota1 = $_POST ['N1'];
 $Nota2 = $_POST ['N2'];
 $Nota3 = $_POST ['N3'];
 $soma = $N1+$N2+$N3;
-$media =  $soma*3;
+$media =  $soma *3;
 $final = $media;
 
-echo "O aluno está".$final;
+echo "O aluno está:".$final;
 
-if {($final>=7 = ("Aprovado"))};
+if {($final>=7)
+"Aprovado"};
 
-
-else {($final<=7 =("Reprovado"))};
+else {($final<=7)
+"Reprovado"};
    
 ?>
